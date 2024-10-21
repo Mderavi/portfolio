437e04cdc24d1a645b87f633fea7291c30fee1d1
M.Deravi <35335419+Mderavi@users.noreply.github.com>
2024-10-21 12:28:39 +1100

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
-import './App.scss';
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
-import Skills from './components/Skills/Skills';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function App() {
-  return (
-    <>
-    <Navbar />
-    <div className="container">
-     <Hero />
-     <Skills />
-    </div>
-    </>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-export default App;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
