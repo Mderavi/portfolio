abca9bbe592260a9ea8a4de1dd4efa3342788e28
Mderavi <deravi.mahsa@gmail.com>
2024-11-29 19:01:35 +1100

    add the forlder and data for work experience.

--- src/utils/data.js
+++ src/utils/data.js
@@ -39,3 +39,37 @@ export const SKILLS = [
     ],
   },
 ];
+
+export const WORK_EXPERIENCE = [
+  {
+    title: "Front-End Web Developer at Within Vision",
+    date: "July.2020 - Current",
+    responsibilities: [
+      "Engaged in technical discussions, shared knowledge with the stakeholders (digital team and colleagues) and participated in the peer code review",
+      "Collaborated with cross-functional teams to define and implement innovative front-end solutions and web components that improved overall user experience.",
+      "Developed responsive and cross-browser compatible web components, contributing to a seamless user experience across multiple platforms and devices.",
+      "Transformed wireframes and UX designs into dynamic web applications using HTML, CSS, and JavaScript, enhancing the online presence and accessibility for clients.",
+    ],
+  },
+  {
+    title: "Front-End Web Developer at Alida",
+    date: "Sep 2014 - Jan 2020",
+    responsibilities: [
+      "Developed and maintained scalable web applications using JavaScript, HTML, and CSS, adhering to best coding practices and standards.",
+      "Addressed cross-browser compatibility issues, ensuring a seamless user experience across various platforms and devices.",
+      "Continuously improved, measured, and evaluated code effectiveness and re-usability. ",
+      "ollaborated across teams to analyze requirements, design solutions, and manage end-to-end project delivery, ensuring alignment with client objectives.",
+    ],
+  },
+  {
+    title: "Front-End Web Developer at Imagination",
+    date: "Aug 2013 - Aug 2014",
+    responsibilities: [
+      "Developed and maintained scalable web applications using JavaScript, HTML, and CSS, adhering to best coding practices and standards.",
+      "Worked on HTML and CSS to complement Facebook applications.",
+      "Built responsive interface from PSD focused. ",
+      "Assisted in building reusable code and libraries for future use.",
+      "Participated in code reviews and collaborated with the development team to ensure high-quality code and adherence to project requirements.",
+    ],
+  },
+];
