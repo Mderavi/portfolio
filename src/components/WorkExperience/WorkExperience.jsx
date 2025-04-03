da545e19f6b86c15c1efc52a6985c380cd12db08
Mderavi <deravi.mahsa@gmail.com>
2025-04-03 14:19:25 +1100

    experince slide added with arrows place holder

--- src/components/WorkExperience/WorkExperience.jsx
+++ src/components/WorkExperience/WorkExperience.jsx
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./WorkExperience.css";
 import ExperianceCard from "./ExperianceCard/ExperianceCard";
 import { WORK_EXPERIENCE } from "../../utils/data";
+import Slider from "react-slick/lib/slider";
 
 const WorkExperience = () => {
+  const sliderRef = useRef();
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <section className="experiance-container">
       <h5>Work Experience</h5>
       <div className="experience-content">
-        {WORK_EXPERIENCE.map((item) => (
-          <ExperianceCard key={item.title} details={item} />
-        ))}
+        <div className="arrow-right">
+          <span className="material-sympols-outlined">
+            chevron-right
+            <i class="fa fa-chevron-right" aria-hidden="true"></i>
+          </span>
+        </div>
+        <div className="left-right">
+          <span className="material-sympols-outlined">
+            chevron-left
+            <i class="fa fa-chevron-left" aria-hidden="true"></i>
+          </span>
+        </div>
+      </div>
+
+      <div className="experience-content">
+        <Slider ref={sliderRef} {...settings}>
+          {WORK_EXPERIENCE.map((item) => (
+            <ExperianceCard key={item.title} details={item} />
+          ))}
+        </Slider>
       </div>
     </section>
   );
