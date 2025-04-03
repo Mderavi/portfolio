966b217b17585c034ae4b3afeb70fed10660eb1b
Mderavi <deravi.mahsa@gmail.com>
2025-04-03 13:46:35 +1100

    adding react silk and work experience placeloder

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,41 +1,42 @@
-import React from 'react'
-import './Hero.scss'
+import React from "react";
+import "./Hero.scss";
 
 const Hero = () => {
   return (
-   <section className="hero-container">
-    <div className="hero-content">
-    <h2>what do you do?</h2>
-    <p>what do you like?d</p>
-    </div>
-      <div className='hero-img'>
+    <section className="hero-container">
+      <div className="hero-content">
+        <h2>what do you do?</h2>
+        <p>
+          I’m a front end developer based in sunny Sydney, Australia. I enjoy
+          turning complex problems into the simple, beautiful and intuitive
+          webpage. When I’m not coding, you’ll find me cooking, gardening or
+          jugging out in the park.
+        </p>
+      </div>
+      <div className="hero-img">
         <div>
-          <div className='tech-icon'>
-            <img src='./assets/img/react-e.png' alt=""/>
+          <div className="tech-icon">
+            <img src="./assets/img/react-e.png" alt="" />
           </div>
 
-
-          <img src='./assets/img/md.jpg' alt='Front end developer'/>
-
+          <img src="./assets/img/md.jpg" alt="Front end developer" />
         </div>
         <div>
-            <div className='tech-icon'>
-                <img src='./assets/img/css-e.png' alt="css icon"/>
-            </div>
-      
-     
-            <div className='tech-icon'>
-                <img src='./assets/img/html-5-e.png' alt="html 5 icon"/>
-            </div>
-      
-        
-            <div className='tech-icon'>
-                <img src='./assets/img/js.png' alt="html 5 icon"/>
-            </div>
+          <div className="tech-icon">
+            <img src="./assets/img/css-e.png" alt="css icon" />
+          </div>
+
+          <div className="tech-icon">
+            <img src="./assets/img/html-5-e.png" alt="html 5 icon" />
+          </div>
+
+          <div className="tech-icon">
+            <img src="./assets/img/js.png" alt="html 5 icon" />
+          </div>
         </div>
       </div>
-   </section>
-  )
-}
+    </section>
+  );
+};
 
 export default Hero;
--- src/components/WorkExperience/ExperianceCard/ExperianceCard.jsx
+++ src/components/WorkExperience/ExperianceCard/ExperianceCard.jsx
@@ -1,8 +1,18 @@
 import React from "react";
-import "./ExperianceCard.css";
+import "./ExperianceCard.scss";
 
 const ExperianceCard = ({ details }) => {
-  return <div>ExperianceCard</div>;
+  return (
+    <div className="work-experience-card">
+      <h6>{details.title}</h6>
+      <div className="work-duration">{details.date}</div>
+      <uL>
+        {details.responsibilities.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </uL>
+    </div>
+  );
 };
 
 export default ExperianceCard;
