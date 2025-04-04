9b866fa5f14e6c6fcaaf4e41f6f61f8e16961e89
Mderavi <deravi.mahsa@gmail.com>
2025-04-05 00:48:49 +1100

    add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Footer.scss";
+const Footer = () => {
+  return <div className="footer"> &copy; 2025 Mahsa Deravi, Happy Coding</div>;
+};
+
+export default Footer;
